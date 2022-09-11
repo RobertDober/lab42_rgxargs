884968625d6fc26948f907302ee74aa72d8f8b74
RobertDober <robert.dober@gmail.com>
2022-09-11 18:07:11 +0000

    fixed CI for 0.3.0

--- lib/lab42.rb
+++ lib/lab42.rb
@@ -1,4 +1,4 @@
-# Our namespace...
+require 'l42/map'
 module Lab42
   require_relative 'lab42/rgxargs'
   # Nothing to be seen here (you feel a slight breeze from the NE)
--- lib/lab42/rgxargs/version.rb
+++ lib/lab42/rgxargs/version.rb
@@ -1,5 +1,3 @@
-require 'l42/map'
-
 module Lab42
   class Rgxargs
     VERSION = "0.3.1"
